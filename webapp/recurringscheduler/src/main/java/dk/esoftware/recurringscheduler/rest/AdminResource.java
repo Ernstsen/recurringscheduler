c9222320e147322912fc15fbc6890e0bfe9514e1
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2023-11-05 14:29:33 +0100

    Moved initialization and overview endpoints to admin rest interface

--- webapp/recurringscheduler/src/main/java/dk/esoftware/recurringscheduler/rest/AdminResource.java
+++ webapp/recurringscheduler/src/main/java/dk/esoftware/recurringscheduler/rest/AdminResource.java
@@ -1,6 +1,7 @@
 package dk.esoftware.recurringscheduler.rest;
 
 import dk.esoftware.recurringscheduler.domain.Overview;
+import dk.esoftware.recurringscheduler.persistence.DefaultInitializationUtility;
 import dk.esoftware.recurringscheduler.persistence.EventType;
 import dk.esoftware.recurringscheduler.persistence.RecurConfig;
 import jakarta.inject.Inject;
@@ -8,21 +9,33 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Root;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.List;
 
-@Path("/overview")
-public class OverviewResource {
+@Path("/admin")
+public class AdminResource {
 
     @Inject
     EntityManager entityManager;
 
+    @POST
+    @Produces(MediaType.TEXT_PLAIN)
+    @Transactional
+    @Path("/init")
+    @Consumes
+    public String initialize() {
+        DefaultInitializationUtility.InitializeStorageWithDefaults(entityManager);
+
+        return "Ensured proper initialization";
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    @Path("/overview")
     public Overview getOverview() {
         return new Overview(
                 getAll(EventType.class),
@@ -39,5 +52,8 @@ public class OverviewResource {
 
         return entityManager.createQuery(cr).getResultList();
     }
+
+
+
 
 }
