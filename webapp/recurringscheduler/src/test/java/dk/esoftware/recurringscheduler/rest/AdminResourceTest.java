39a70da01c1e799cdac557adaf85c08a61ff443a
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2023-11-05 14:46:51 +0100

    create test of admin resource

---
+++ webapp/recurringscheduler/src/test/java/dk/esoftware/recurringscheduler/rest/AdminResourceTest.java
@@ -0,0 +1,29 @@
+package dk.esoftware.recurringscheduler.rest;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class AdminResourceTest {
+
+    @Test
+    void testOverviewEndpointPostInitialization() {
+        given()
+                .when().post("/admin/init")
+                .then().statusCode(200)
+                .body(is("Ensured proper initialization"));
+
+        given()
+                .when().get("/admin/overview")
+                .then()
+                .body(
+                        containsString("Once a week"),
+                        containsString("Once a month"),
+                        containsString("Twice a year")
+                );
+    }
+}
