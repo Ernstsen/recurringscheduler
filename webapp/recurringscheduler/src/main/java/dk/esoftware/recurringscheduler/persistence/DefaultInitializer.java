ff36a07434981fbb038c5db8fcd7ca126ab29892
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2023-12-22 12:15:03 +0100

    Fixed init flow

--- webapp/recurringscheduler/src/main/java/dk/esoftware/recurringscheduler/persistence/DefaultInitializer.java
+++ webapp/recurringscheduler/src/main/java/dk/esoftware/recurringscheduler/persistence/DefaultInitializer.java
@@ -2,6 +2,7 @@ package dk.esoftware.recurringscheduler.persistence;
 
 import io.quarkus.runtime.Startup;
 import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,8 +11,9 @@ public class DefaultInitializer {
     private static final Logger logger = LoggerFactory.getLogger(DefaultInitializer.class);
 
     @Inject
-    public DefaultInitializer() {
-        logger.info("This should theoretically initialize the server");
+    public DefaultInitializer(EntityManager em) {
+        DefaultInitializationUtility.InitializeStorageWithDefaults(em);
+        logger.info("Successfully initialized DB with default values");
     }
 
 
