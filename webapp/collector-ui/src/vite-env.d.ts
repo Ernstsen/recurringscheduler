d785737b451cbe10fe19cf042c43e19fdedd7722
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2024-02-16 20:23:49 +0100

    Created basis for ui

--- webapp/collector-ui/src/vite-env.d.ts
+++ webapp/collector-ui/src/vite-env.d.ts
@@ -1,1 +1,14 @@
-/// <reference types="vite/client" />
+import {defineConfig} from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+        plugins: [react()],
+        server: {
+            host: "localhost",
+            proxy: {
+                "^/api/*": "http://localhost:8080"
+            }
+        }
+    }
+)
