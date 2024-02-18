45485291b5c4bbf46faa8f78f56f962b8b83eb07
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2024-02-18 21:51:45 +0100

    Introduced react-router-dom for collect keys

--- webapp/collector-ui/src/App.tsx
+++ webapp/collector-ui/src/App.tsx
@@ -1,24 +1,17 @@
 import React, {useState} from 'react'
 import {Paper} from "@mui/material";
 import styles from "./App.module.css";
+import {useParams} from "react-router-dom";
 
 function App() {
-    const [count, setCount] = useState(0)
+    let {collectId} = useParams();
 
     return (
         <React.Fragment>
             <Paper className={styles.Paper} elevation={10} variant={"elevation"}>
-                <h1>Vite + React</h1>
-                <div className="card">
-                    <button onClick={() => setCount((count) => count + 1)}>
-                        count is {count}
-                    </button>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save to test HMR
-                    </p>
-                </div>
-                <p className="read-the-docs">
-                    Click on the Vite and React logos to learn more
+                <h1>Collector</h1>
+                <p>
+                    Collecting data for {collectId}
                 </p>
             </Paper>
         </React.Fragment>
