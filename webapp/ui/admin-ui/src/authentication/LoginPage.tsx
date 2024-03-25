da216aaee89f7d23d433deec5b27d7a3029e83f9
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2024-03-25 10:42:39 +0100

    Small improvements to be build-pipeline ready

--- webapp/ui/admin-ui/src/authentication/LoginPage.tsx
+++ webapp/ui/admin-ui/src/authentication/LoginPage.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import {Alert, Button, Grid, Paper, TextField, Typography} from '@mui/material';
 import {AuthControl, useAuth} from "./UseAuthentication.tsx";
-import useStyles from './LoginPage.module.css';
+import styles from './LoginPage.module.css';
 import {useNavigate} from "react-router-dom";
 import Box from "@mui/material/Box";
 import imgUrl from './logo_transparent.png';
@@ -14,9 +14,9 @@ const LoginPage: React.FC = () => {
     let navigate = useNavigate();
 
     return (
-        <Grid container className={useStyles.container} justifyContent="center" alignItems="center">
+        <Grid container className={styles.container} justifyContent="center" alignItems="center">
             <Grid item xs={12} sm={8} md={6}>
-                <Paper className={useStyles.paper} elevation={3} sx={{padding: "2vw"}}>
+                <Paper className={styles.paper} elevation={3} sx={{padding: "2vw"}}>
                     <Box sx={{width: 200, height: 200, margin: "auto"}}
                          component={"img"}
                          src={imgUrl}
@@ -54,7 +54,7 @@ const LoginPage: React.FC = () => {
                         fullWidth
                         variant="contained"
                         color="primary"
-                        className={useStyles.submitButton}
+                        className={styles.submitButton}
                         onClick={() => {
                             authMemory.login(email, password)
                                 .then(() => {
--- webapp/ui/collector-ui/src/vite-env.d.ts
+++ webapp/ui/collector-ui/src/vite-env.d.ts
@@ -1,14 +1,1 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-        plugins: [react()],
-        server: {
-            host: "localhost",
-            proxy: {
-                "^/api/*": "http://localhost:8080"
-            }
-        }
-    }
-)
+/// <reference types="vite/client" />
