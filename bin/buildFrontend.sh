4436231cf2dc0628a4b97c42eea9bb716ba08d8c
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2024-03-28 07:57:02 +0100

    Introduced shared version properties file for global versioning

--- bin/buildFrontend.sh
+++ bin/buildFrontend.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+source version.properties
+
+if [ -z "${version}" ]; then
+  echo "Version is not set in version.properties"
+  exit 1
+fi
+
+# shellcheck disable=SC2154
+echo "Building frontend with version: ${version}"
+
 cd webapp/ui/admin-ui || exit
 npm ci
 npm run build
@@ -11,4 +21,4 @@ npm run build
 cd ../../../
 
 cd webapp/ui || exit
-docker build -t ernstsen/recurringschedulerfrontend .
+docker build -t ernstsen/recurringschedulerfrontend:"${version}" .
