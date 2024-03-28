407fac64a6beb3336e972390f188c78f13661e22
Johannes Ernstsen <ernstsen.johannes@gmail.com>
2024-03-28 12:27:02 +0100

    Now build frontend in docker builder-stage

--- bin/buildFrontend.sh
+++ bin/buildFrontend.sh
@@ -10,15 +10,10 @@ fi
 # shellcheck disable=SC2154
 echo "Building frontend with version: ${version}"
 
-cd webapp/ui/admin-ui || exit
-npm ci
-npm run build
-cd ../../../
-
-cd webapp/ui/collector-ui || exit
-npm ci
-npm run build
-cd ../../../
+#cd webapp/ui/collector-ui || exit
+#npm ci
+#npm run build
+#cd ../../../
 
 cd webapp/ui || exit
 docker build -t ernstsen/recurringscheduler-frontend:"${version}" .
